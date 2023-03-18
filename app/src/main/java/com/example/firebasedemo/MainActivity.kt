b0fff425d7610cbfa87d4b630b1bdc62ea5bb5be
HiepPD <hieppdutck59@gmail.com>
2023-03-18 21:12:10 +0700

    Update Crashlytics in  Engage

--- app/src/main/java/com/example/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/example/firebasedemo/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.firebasedemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // test crashlytics
+        eventCrashlytics()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun eventCrashlytics() {
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
